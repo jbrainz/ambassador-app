04fcfa77320351bf27d8774a837f9c96f1544803
jbrainz <developer@roberronero.com>
2021-07-03 22:51:53 +0100

    ambassador route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('admin/ambassadors')
+  async ambassador() {
+    return this.userService.find({
+      is_ambassador: true,
+    });
+  }
+}
