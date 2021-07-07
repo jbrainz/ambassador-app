57ff171f6b7231edd8a22eb98a4c772553c1632b
jbrainz <developer@roberronero.com>
2021-07-07 16:09:55 +0100

    ...

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { AuthGaurd } from 'src/auth/auth.gaurd';
+import { LinkService } from './link.service';
 
 @Controller('link')
-export class LinkController {}
+export class LinkController {
+  constructor(private linkService: LinkService) {}
+
+  @UseGuards(AuthGaurd)
+  @Get('admin/user/:id/links')
+  async all(@Param('id') id: number) {
+    return this.linkService.find({
+      user: id,
+      relations: ['orders'],
+    });
+  }
+}
